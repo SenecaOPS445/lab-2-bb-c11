300fc6d9b2e1ffe245e6b577955b3d9999314752
Bishwash BC <bb-c1@Bishwash>
2024-05-28 23:12:34 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author:[Bishwash BC]
+# Author ID: [184751212]
+# Date Created: [2024/05/28]
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
